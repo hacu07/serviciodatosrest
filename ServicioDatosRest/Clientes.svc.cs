7c318446249384ef25807ceedf3cc6f89b0f1a12
Harold A Cupitra Hernandez <hacu12@hotmail.com>
2020-04-06 09:27:48 -0500

    Agregar archivos de proyecto.

---
+++ ServicioDatosRest/Clientes.svc.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Web.Script.Serialization;
+
+namespace ServicioDatosRest
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Clientes" en el código, en svc y en el archivo de configuración a la vez.
+    // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Clientes.svc o Clientes.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class Clientes : IClientes
+    {
+        public List<Cliente> repo;
+
+        public Clientes()
+        {
+            repo = new List<Cliente>();
+            repo.Add(new Cliente() { Id = 1, Mail = "mail1@google.com", Nombre = "Cliente1", Telefono = "1111" });
+            repo.Add(new Cliente() { Id = 2, Mail = "mail2@google.com", Nombre = "Cliente2", Telefono = "2222" });
+            repo.Add(new Cliente() { Id = 3, Mail = "mail3@google.com", Nombre = "Cliente3", Telefono = "3333" });
+            repo.Add(new Cliente() { Id = 4, Mail = "mail4@google.com", Nombre = "Cliente4", Telefono = "4444" });
+        }
+
+        public Cliente Create(Cliente cliente)
+        {
+            if (cliente != null)
+            {
+                if (cliente.Id == 0)
+                {
+                    int maxCliente = repo.Max(c => c.Id);
+                    cliente.Id = maxCliente + 1;
+                }
+                repo.Add(cliente);
+            }
+            return cliente;
+        }
+
+        public void Delete(int idCliente)
+        {
+            if (idCliente > 0)
+            {
+                Cliente cli = ObtenerCliente(idCliente);
+                if (cli != null)
+                {
+                    repo.Remove(cli);
+                }
+            }
+        }
+
+        public Cliente Get(string idCliente)
+        {
+            int id = 0;
+            int.TryParse(idCliente, out id);
+            return ObtenerCliente(id);
+        }
+
+        private Cliente ObtenerCliente(int idCliente)
+        {
+            Cliente cli = repo.Where(c => c.Id == idCliente).Select(c => c).SingleOrDefault();
+            return cli;
+        }
+
+        public List<Cliente> GetAll()
+        {
+            return repo;
+        }
+
+        public Cliente Update(Cliente cliente)
+        {
+            Cliente cli = null;
+            if (cliente !=null)
+            {
+                cli = ObtenerCliente(cliente.Id);
+                if (cli != null)
+                {
+                    cli.Nombre = cliente.Nombre;
+                    cli.Mail= cliente.Mail;
+                    cli.Telefono = cliente.Telefono;
+                }
+            }
+            return cli;
+        }
+    }
+}
---
+++ ServicioDatosRest/IClientes.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ServicioDatosRest
+{
+    [DataContract]
+    public class Cliente
+    {
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public string Nombre { get; set; }
+        [DataMember]
+        public string Telefono { get; set; }
+        [DataMember]
+        public string Mail { get; set; }
+    }
+
+    [ServiceContract]
+    public interface IClientes
+    {
+        [OperationContract]
+        [WebGet(UriTemplate = "/getAll",ResponseFormat = WebMessageFormat.Json)]
+        List<Cliente> GetAll();
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/get/{idCliente}", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped)]
+        Cliente Get(string idCliente);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "/create", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, Method ="POST")]
+        Cliente Create(Cliente cliente);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "/update", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, Method = "POST", BodyStyle = WebMessageBodyStyle.Wrapped)]
+        Cliente Update(Cliente cliente);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "/delete", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped, Method = "POST")]
+        void Delete(int idCliente);
+    }
+}
